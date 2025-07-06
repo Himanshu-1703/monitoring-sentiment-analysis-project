3c338fb892083dd1e6a3c637841ecb4ec160d287
Himanshu <himanshu1703@gmail.com>
2025-07-06 18:22:37 +0530

    changed dagshub credentials

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -13,8 +13,8 @@ def promote_model():
     os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
     dagshub_url = "https://dagshub.com"
-    repo_owner = "campusx-official"
-    repo_name = "mlops-mini-project"
+    repo_owner = "himanshu1703"
+    repo_name = "monitoring-sentiment-analysis-project"
 
     # Set up MLflow tracking URI
     mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
--- tests/test_model.py
+++ tests/test_model.py
@@ -20,8 +20,8 @@ class TestModelLoading(unittest.TestCase):
         os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
         dagshub_url = "https://dagshub.com"
-        repo_owner = "campusx-official"
-        repo_name = "mlops-project-2"
+        repo_owner = "himanshu1703"
+        repo_name = "monitoring-sentiment-analysis-project"
 
         # Set up MLflow tracking URI
         mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
